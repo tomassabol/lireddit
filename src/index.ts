5e84ead4792fa74284fd9b82fd65ec6693d03307
Tomáš Sabol <tomassabol2@gmail.com>
2023-01-31 19:19:48 +0100

    migration and mikroorm config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello World');
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async() => {
+    const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
+    const post = orm.em.fork().create(Post, {
+        title: "My first post",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    });
+    await orm.em.persistAndFlush(post);
+};
+
+main().catch((err) => {
+    console.error(err);
+});
